d43085049cbb1e60ab5d2b5c187a6441fcaf95bd
Adam Howard <skattyadz+gh@gmail.com>
2019-02-13 20:56:47 -0800

    Convert to now-jest

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# now-static-build-storybook
+# now-jest
 
 This package provides a
 [builder](https://zeit.co/docs/v2/deployments/builders/overview#when-to-use-builders)
-for Zeit's [Now 2.0](https://zeit.co/blog/now-2) that compiles [Storybooks](https://github.com/storybooks/storybook)
+for Zeit's [Now 2.0](https://zeit.co/blog/now-2) that runs [Jest](https://jestjs.io/) tests
 
-`now-static-build` always runs the `now-build` script, but we wanted to reuse our the `package.json` file to deploy a storybook
+`now-static-build` always runs the `now-build` script, but we wanted to reuse our `package.json` for multiple builds
 
 Based on https://github.com/zeit/now-builders/tree/master/packages/now-static-build
 
@@ -19,19 +19,19 @@ Your `now.json` `"builds"` section should look something like this:
 {
   "builds": [
     {
-      "src": ".storybook/config.js",
-      "use": "now-static-build-storybook"
+      "src": "jest.config.js",
+      "use": "now-build-jest"
     }
   ]
 }
 ```
 
-Your `package.json` needs a script named `now-build-storybook`. By default the `dist` directory gets merged with your site, so `dist/storybook` will mount it at `/storybook`
+Your `package.json` needs a script named `now-build-jest`. I'm sure we could remove this step and call jest directly
 
 ```json
 {
   "scripts": {
-    "now-build-storybook": "build-storybook -o dist/storybook"
+    "now-build-jest": "jest --ci --bail"
   }
 }
 ```
--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 const download = require("@now/build-utils/fs/download.js"); // eslint-disable-line import/no-extraneous-dependencies
-const glob = require("@now/build-utils/fs/glob.js"); // eslint-disable-line import/no-extraneous-dependencies
 const path = require("path");
-const { existsSync, readdirSync } = require("fs");
 const {
   runNpmInstall,
   runPackageJsonScript
 } = require("@now/build-utils/fs/run-user-scripts.js"); // eslint-disable-line import/no-extraneous-dependencies
 
-function validateDistDir(distDir) {
-  if (!existsSync(distDir)) {
-    const distDirName = path.basename(distDir);
-    const message =
-      `Build was unable to create the distDir: ${distDirName}.` +
-      "\nMake sure you mentioned the correct dist directory: https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build/#configuring-the-dist-directory";
-    throw new Error(message);
-  }
-}
-
 exports.build = async ({ files, entrypoint, workPath, config }) => {
-  if (path.basename(entrypoint) !== "config.js") {
-    throw new Error("Storybook config.js must be specified as entrypoint");
+  if (path.basename(entrypoint) !== "jest.config.js") {
+    throw new Error("jest.config.js must be specified as entrypoint");
   }
 
   console.log("downloading user files...");
   await download(files, workPath);
 
-  console.log("running now-build-storybook script...");
-  const mountpoint = path.join(entrypoint, "..", "..");
+  console.log("running now-build-jest script...");
+  const mountpoint = path.dirname(entrypoint);
   const entrypointFsDirname = path.join(workPath, mountpoint);
 
   await runNpmInstall(entrypointFsDirname, ["--prefer-offline"]);
 
-  if (await runPackageJsonScript(entrypointFsDirname, "now-build-storybook")) {
-    const distPath = path.join(
-      workPath,
-      mountpoint,
-      (config && config.distDir) || "dist"
-    );
-    validateDistDir(distPath);
-    return glob("**", distPath, mountpoint);
+  if (await runPackageJsonScript(entrypointFsDirname, "jest --ci --bail")) {
+    return {};
   }
-  throw new Error(`An error running "now-build" script in "${entrypoint}"`);
+
+  throw new Error(
+    `An error running "now-build-jest" script in "${entrypoint}"`
+  );
 };
